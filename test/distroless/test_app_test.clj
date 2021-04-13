dfec5c2f1ff8e5a70b5867c0ab46b65b99fd33dd
Jim Clark <jim@atomist.com>
2021-04-13 12:56:15 -0700

    Initial Commit

---
+++ test/distroless/test_app_test.clj
@@ -0,0 +1,7 @@
+(ns distroless.test-app-test
+  (:require [clojure.test :refer :all]
+            [distroless.test-app :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
